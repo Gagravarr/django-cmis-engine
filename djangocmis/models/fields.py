e62d70bc63aae6f6cd0da976962cfba2f4905e87
Nick Burch <nick@gagravarr.org>
2014-10-07 16:41:02 +0100

    Two possible ways to do the extra values

--- djangocmis/models/fields.py
+++ djangocmis/models/fields.py
@@ -20,7 +20,36 @@ class CharField(fields.CharField):
           kwargs['max_length'] = 200
        super(CharField, self).__init__(*args, **kwargs)
 
+_ContentField_name = "cmis:contentStream"
+class ContentField(fields.Field):
+   "A CMIS Content Stream"
+   name = "cmis:contentStream"
+   def __init__(self, *args, **kwargs):
+      super(ContentField, self).__init__(*args, **kwargs)
+      self.name = _ContentField_name
+      self.db_column = self.name
+      self.verbose_name = self.name
+      self.rel = None
+      self.default = None
+      self._choices = []
+      self.primary_key = False
+   pass
+
+_CMISObjectField_name = "cmis:object"
+class CMISObjectField(fields.Field):
+   "Special field to hold the CMIS Object from cmislib"
+   def __init__(self, *args, **kwargs):
+      kwargs["db_column"] = _CMISObjectField_name
+      super(CMISObjectField, self).__init__(*args, **kwargs)
+      #self.db_column = self.name
+      #self.verbose_name = self.name
+      #self.rel = None
+      #self.default = None
+      #self._choices = []
+      #self.primary_key = False
+
 class ParentField(fields.Field):
-    pass
+   "Special field for representing the parent of a model"
+   pass
 
 # TODO The rest
